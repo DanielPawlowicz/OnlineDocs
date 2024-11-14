e4dc67ddff3f71cb0d8b07e765c493a1598ae33f
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-11-14 11:11:56 +0100

    Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import TextEditor from './TextEditor'
+import './App.css';
+import TextEditor from './TextEditor';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
-
   return (
-    <>
-    <TextEditor />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={`/documents/${uuidV4()}`} replace />}
+        />
+        <Route
+          path="/documents/:id"
+          element={<TextEditor />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
